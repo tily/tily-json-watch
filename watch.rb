aa5aa6b1a25117bfcda8ebfccf1d12e77f7ebf74
tily <tily05@gmail.com>
2014-07-27 14:21:19 +0900

    add watch script

---
+++ watch.rb
@@ -0,0 +1,58 @@
+require 'json_watch'
+require 'redis'
+require 'NIFTY'
+require 'mail'
+
+Mail.defaults do
+  delivery_method :smtp, {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
+
+class Watch < JsonWatch
+	%w(east-1 west-1).each do |region|
+		watch :"#{region} instances", notify: [:stdout, :mail], exclude: 'requestId' do
+			compute(region).describe_instances.to_hash
+		end
+	end
+
+	notify :stdout do |watch, diff|
+		STDOUT.puts title(watch)
+		STDOUT.puts JSON.pretty_generate(diff)
+	end
+
+	notify :mail do |watch, diff|
+		Mail.deliver do
+		  to ENV['TO_MAIL_ADDRESS']
+		  from 'tily-watch@herokuapp.com'
+		  subject title(watch)
+		  body JSON.pretty_generate(diff)
+		end
+	end
+
+	class << self
+		def title(watch)
+			"#{watch[:name]} changed!"
+		end
+
+		def compute(region)
+			api = NIFTY::Cloud::Base.new(
+				access_key: ENV['ACCESS_KEY_ID'],
+				secret_key: ENV['SECRET_ACCESS_KEY'],
+				server: "#{region}.cp.cloud.nifty.com",
+				path: '/api',
+				socket_timeout: 300,
+				connection_timeout: 300
+			)
+		end
+	end
+end
+
+watch = Watch.new(cache: Redis.new, sleep: 60*5)
+watch.start
